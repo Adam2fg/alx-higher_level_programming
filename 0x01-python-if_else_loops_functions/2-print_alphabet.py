6f5523823856aad1a472d8a5905a12dd4429bb0d
Adam Abah <danielabah45@gmail.com>
2022-11-30 07:58:38 +0100

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 """Print the alphabet in lowercase, not followed ba new line."""
+
 for letter in range(97, 123):
         print("{}".format(chr(letter)), end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for character in range(97, 123):
-        if (character != 101 and character != 113):
-                    print("{:c}".format(character), end='')
+for letter in range(97, 123):
+        if chr(letter) != 'q' and chr(letter) != 'e':
+                    print("{}".format(chr(letter)), end='')
