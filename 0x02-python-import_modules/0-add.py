cfba1f2cbc99607eccaa3ac394fc789a426d354d
Adam Abah <danielabah45@gmail.com>
2022-12-07 20:29:07 -0800

    task done

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-def add(1, 2):
-    return (1 + 2)
-=======
+
 if __name__ == "__main__":
-        """Print the sum of 1 and 2."""
-        from add_0 import add
-            a = 1
-                b = 2
-                    print("{} + {} = {}".format(a, b, add(a, b)))
->>>>>>> 188ca637c28a23e274b55bfa4820a1a26edda56a
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
