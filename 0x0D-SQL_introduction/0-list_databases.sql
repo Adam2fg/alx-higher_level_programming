9760ca6c5ec7ad02b6e1f726e8e6a1bd7b551181
Adam Abah <danielabah45@gmail.com>
2023-02-15 21:27:27 -0800

    task done

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,12 +1,2 @@
-#!/bin/bash
-
-# Set MySQL credentials
-MYSQL_USER="your_mysql_username"
-MYSQL_PASSWORD="your_mysql_password"
-
-#Get a list of all databases in the MySQL server
-databases=$(mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)")
-
-# Print the list of databases
-echo "List of databases:"
-echo "$databases"
+# ls all databases in mySQL Server
+SHOW DATABASES;
