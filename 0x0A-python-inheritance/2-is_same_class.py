fdbdd2622c9ab76046ec68fb357937ab63a47f4c
Adam Abah <danielabah45@gmail.com>
2023-04-19 17:23:41 +0100

    task done

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines a class-checking function."""
 
-def is_same_class(obj. a_class):
-    """"Check if an object is exactly an instance of a given class.
-    
+
+def is_same_class(obj, a_class):
+    """Check if an object is exactly an instance of a given class.
     Args:
         obj (any): The object to check.
         a_class (type): The class to match the type of obj to.
